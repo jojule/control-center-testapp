6741bc82a97f05b29e7ffd032c3db3f38c0732e3
Joonas Lehtinen <644969+jojule@users.noreply.github.com>
2024-09-29 15:16:39 -0700

    Organizing imports

--- src/main/java/com/example/application/views/DefaultView.java
+++ src/main/java/com/example/application/views/DefaultView.java
@@ -1,7 +1,6 @@
 package com.example.application.views;
 
 import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
--- src/main/java/com/example/application/views/PrivateView.java
+++ src/main/java/com/example/application/views/PrivateView.java
@@ -1,13 +1,11 @@
 package com.example.application.views;
 
 import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.auth.AnonymousAllowed;
 import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
+
 import jakarta.annotation.security.PermitAll;
 
 @PageTitle("Private")
